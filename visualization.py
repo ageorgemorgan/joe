d876b8f55832b6942e5f220969b4b3607f3eafa5
Adam George Morgan <adam.morgan@mail.utoronto.ca>
2023-11-20 10:52:13 -0500

    bandaid fix for TeX issues
    Someone found that their tex install doesn't like to play nicely with matplotlib. For now I've put a band-aid on the problem: if the computer can't find tex, they use a default matplotlib font. This results in OK but not great pictures, but it's all I can do RN. Also, without latex the combo movies where I plot the power spectrum will really look bad, so I need a better fix for this down the line.

--- visualization.py
+++ visualization.py
@@ -19,8 +19,16 @@ import sys
 
 
 def hov_plot(x, t, u, fieldname, show_figure=True, save_figure=False, picname="", cmap=cmo.haline):
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
+    # I like to use tex to render the graph labels nicely. However, for the benefit of users who either don't
+    # have tex installed or are encountering some issue where matplotlib doesn't want to talk to tex,
+    # I've introduced the following "out" that just uses a default matplotlib serif font instead (which looks
+    # OK but not that nice)
+    try:
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
+
+    except RuntimeError:
+        plt.rcParams["font.family"] = "serif"
 
     umin = np.amin(u)
     umax = np.amax(u)
@@ -29,19 +37,19 @@ def hov_plot(x, t, u, fieldname, show_figure=True, save_figure=False, picname=""
     CF = plt.contourf(x, t, u, cmap=cmap, levels=levels)
 
     # axis labels
-    plt.xlabel(r"$x$", fontsize=26, color='k')
-    plt.ylabel(r"$t$", fontsize=26, color='k')
+    plt.xlabel(r"$x$", fontsize=22, color='k')
+    plt.ylabel(r"$t$", fontsize=22, color='k')
 
     plt.tick_params(axis='x', which='both', top=False, color='k')
-    plt.xticks(fontsize=20, rotation=0, color='k')
+    plt.xticks(fontsize=16, rotation=0, color='k')
     plt.tick_params(axis='y', which='both', right=False, color='k')
-    plt.yticks(fontsize=20, rotation=0, color='k')
+    plt.yticks(fontsize=16, rotation=0, color='k')
 
     # make colorbar
     cbar = plt.colorbar(CF, format='%.2f')
     cbar.ax.tick_params(labelsize=16, color='k')
     plt.clim(umin, umax)
-    cbar.ax.set_ylabel(fieldname, fontsize=26, color='k')
+    cbar.ax.set_ylabel(fieldname, fontsize=22, color='k')
 
     # the final piece of the colorbar defn is to change the colorbar ticks to an acceptable color.
     # This is not so easy, and relies on the thread at
@@ -81,16 +89,31 @@ def hov_plot(x, t, u, fieldname, show_figure=True, save_figure=False, picname=""
 
 def save_movie(u, x, length, dt, ndump, filename, periodic=True, dpi=100):
     # Create movie file in mp4 format. Warning: this is very slow!
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
+
+    try:
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
+        usetex=True
+
+    except RuntimeError:
+        plt.rcParams["font.family"] = "serif"
+        usetex=False
 
     fig = plt.figure()
 
     umin = 1.05 * np.amin(u)
     umax = 1.05 * np.amax(u)
 
+    #from scipy.integrate import simps
+
+    #coeff = simps(np.abs(u[0, :]),x)  #TODO: multiplying by a factor of 4 gives a solid result , but for BBM 6 is # the
+    # correct "back of the envelope" value.
+
     # TODO: CAREFUL THE xLIM HERE!!!!! MAKE A FUNCTION OF ABSORBING LAYER
     ax = plt.axes(xlim=(-0.5 * length, 0.5 * length), ylim=(umin, umax))
+    #ax = plt.axes(xlim=(-60, 25), ylim=(umin, umax))
+
+    fig.set_size_inches(8, 6)
 
     # use cubic spline interpolation to smooth the data
 
@@ -120,7 +143,7 @@ def save_movie(u, x, length, dt, ndump, filename, periodic=True, dpi=100):
 
     # now we can actually do the upsampling
 
-    NN = 600  # number of points to evaluate interpolant at
+    NN = 1200  # number of points to evaluate interpolant at
 
     xx = np.linspace(-0.5 * length, 0.5 * length, NN, endpoint=True)
 
@@ -131,27 +154,54 @@ def save_movie(u, x, length, dt, ndump, filename, periodic=True, dpi=100):
     # u = uu
 
     ax.grid('True')
-    ax.set_xlabel('$x$', fontsize=22)
 
     if periodic:
 
-        color = 'xkcd:ocean green'
+        # color = 'xkcd:ocean green'
+        color = 'xkcd:cerulean'
 
     else:
 
         color = 'xkcd:dark magenta'
 
-    line, = ax.plot([], [], linewidth=2, color=color)
+    line, = ax.plot([], [], linewidth=2, color=color, label='$u(t,x)$')
+    #bound_line_up, = ax.plot([], [], linewidth=2, color='xkcd:emerald', linestyle='dashed',
+                            # label='$\|u_0\|_{L^{1}_{x}} (1+t)^{-1/3}$')
+    #bound_line_down, = ax.plot([], [], linewidth=2, color='xkcd:emerald', linestyle='dashed')
 
     timer = fig.canvas.new_timer(interval=100)
     timer.add_callback(u, ax)
     timer.start()
+
+    #ax.legend(fontsize=20, loc='upper left')
+
+    plt.tick_params(axis='x', which='both', top=False, color='k')
+    plt.xticks(fontsize=20, rotation=0, color='k')
+    plt.tick_params(axis='y', which='both', right=False, color='k')
+    plt.yticks(fontsize=20, rotation=0, color='k')
+
+    plt.xlabel(r"$x$", fontsize=22, color='k')
+
 
     # Animation function.
     def animate(i):
         line.set_data(x, u[i, :])
 
         tplot = i * dt * ndump
+
+        #bound_line_up.set_data(x, coeff / ((1 + tplot) ** (1. / 3.)))
+        #bound_line_down.set_data(x, -coeff / ((1 + tplot) ** (1. / 3.)))
+
+        #plt.title('$t=%.2f$' % tplot, fontsize=22)
+
+        if usetex == True:
+            ylabel_str= '$u(x,t=%.2f)$' % tplot
+
+        elif usetex == False:
+            ylabel_str= 'u(x,t=%.2f)' % tplot
+
+        ax.set_ylabel(ylabel_str, fontsize=22)
+        """
 
         if periodic:
 
@@ -160,17 +210,32 @@ def save_movie(u, x, length, dt, ndump, filename, periodic=True, dpi=100):
         else:
 
             ax.set_ylabel('$\phi(x,t=%.2f)$' % tplot, fontsize=22)
+            
+        """
 
         # TODO: the above label fixes are kind of a jerry-rigged solution and don't generalize easily to other PDE!
         #     Find a smart way to fix this!
 
         plt.xlim([-.5 * length, .5 * length])
+        #plt.xlim = ([-60., 25.])
 
-        plt.tight_layout()
+        """
+        if i%400==0 or i == 800 or i==1000:
+
+            plt.savefig('visuals/' + 'frame=%.1f' %i + '.png', bbox_inches='tight', dpi=600)
+
+        else:
+
+            pass 
+        """
+
+        # plt.tight_layout()
 
         return line,
 
     anim = animation.FuncAnimation(fig, animate, np.shape(u)[0], blit=False)
+
+    #plt.savefig('visuals/' + 'frame'+ '.png', bbox_inches='tight', dpi=100)
 
     # add the folder "visuals" to our path... more on this below
     my_path = os.path.join("visuals")
@@ -179,13 +244,20 @@ def save_movie(u, x, length, dt, ndump, filename, periodic=True, dpi=100):
     if not os.path.isdir(my_path):
         os.makedirs(my_path)
 
-    anim.save('visuals/' + filename, fps=200, extra_args=['-vcodec', 'libx264'], dpi=dpi)
+    anim.save('visuals/' + filename, fps=50, extra_args=['-vcodec', 'libx264'], dpi=dpi)
 
 
 def save_combomovie(u, x, length, dt, ndump, filename, dpi=100):
     # Create movie file in mp4 format. Warning: this is very slow!
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
+
+    try:
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
+        usetex = True
+
+    except RuntimeError:
+        plt.rcParams["font.family"] = "serif"
+        usetex = False
 
     fig = plt.figure()
 
@@ -277,9 +349,17 @@ def save_combomovie(u, x, length, dt, ndump, filename, dpi=100):
 
         tplot = i * dt * ndump
 
-        ax.set_ylabel('$u(x,t=%.2f)$' % tplot, fontsize=22)
+        if usetex == True:
+            ylabel_str= '$u(x,t=%.2f)$' % tplot
 
-        ins.set_ylabel('$|\widehat{u}|^2(k,t=%.2f)$' % tplot, fontsize=11)
+        elif usetex == False:
+            ylabel_str= 'u(x,t=%.2f)' % tplot
+
+        ax.set_ylabel(ylabel_str, fontsize=22)
+
+        # TODO: get this looking better when tex doesn't want to work!
+        in_ylabel_str = '$|\widehat{u}|^2(k,t=%.2f)$' % tplot
+        ins.set_ylabel(in_ylabel_str, fontsize=11)
 
         plt.tight_layout()
 
