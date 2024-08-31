34c17e72f31d54d244f1b6ad5307677087bbb430
Adam George Morgan <adam.morgan@mail.utoronto.ca>
2024-08-30 20:59:31 -0400

    update utils w/ dealiasing
    Utils was updated w/ a helper for computing dealiased powers. NOT IN THE RELEASED VERSION YET!!!! Will do further testing with it and update soon

--- utils.py
+++ utils.py
@@ -2,27 +2,27 @@ import numpy as np
 from scipy.fft import fft, ifft, rfft, irfft
 
 # helper function for efficiently taking fft of real or complex fields
-def my_fft(u, complex=False):
+def my_fft(u, n=None, complex=False):
 
     if complex:
 
-        out = fft(u)
+        out = fft(u, n=n)
 
     else:
 
-        out = rfft(u)
+        out = rfft(u, n=n)
 
     return out
 
-def my_ifft(V, complex=False):
+def my_ifft(V, n=None, complex=False):
 
     if complex:
 
-        out = ifft(V)
+        out = ifft(V, n=n)
 
     else:
 
-        out = irfft(V)
+        out = irfft(V, n=n)
 
     return out
 
@@ -34,3 +34,47 @@ def my_ifft(V, complex=False):
 # since this is a postprocessing func it doesn't need to be optimized for real inputs with rfft.
 def integrate(u, length, N):
     return (length/N) * np.real(fft(u, axis=-1)[..., 0])
+
+# symbol of the lo-pass Orszag a-filter with 0<a<1 (typically a=2/3). Ref: Boyd Ch.11
+def orszag(k, length, N, a=2./3.):
+    km = np.pi*N/length # max freq
+    akm = a*km
+
+    # option a: classical hard transition
+    #out = np.zeros_like(k, dtype=float)
+    #out[abs(k) <= akm] = 1.
+    #out[abs(k) >= akm] = 0.
+
+    # option b: softened transition
+    W = 1e2
+    out = 0.5*(1.-np.tanh(W*(k-akm)))
+
+    return out
+
+def dealiased_pow(V,p):
+    # returns a version of rfft(irfft(V)**p) dealiased via zero-padding
+
+    # Important: this fnc only works for REAL fields bcz for complex fields algebraic nonlinearities typically involve
+    # u* as well as u... so padding for such NL terms should be done on-the-fly.
+
+    N = 2 * (len(V) - 1)
+
+    K = int(0.5*(p+1)*N)
+
+    upad = my_ifft(V, n=K, complex=False)
+
+    # FOR DOC. PURPOSES ONLY: the above lines of code produce the same result as the following block:
+    #Vpad = 1j * np.zeros(int(0.5 * K + 1), dtype=float)
+    #Vpad[0:len(V)] = V
+    #upad = irfft(Vpad, n=K)
+
+    out = ((K/N)**(p-1))*my_fft(upad ** p, complex=False)[0:int(0.5*N+1)]
+
+    # TODO: I discovered the correct normalizations via trial-and error, and by comparing Fu2, Fu3 against
+    #  rfft(irfft(V) ** 2), rfft(irfft(V) ** 3) resp. Make sure to write in a tutorial, or in the docs, a more
+    #  systematic way of determining this normalization. Dealiasing via padding would be a great topic for a
+    #  "tutorial 5"! U could also experiment with filtering vs. padding vs. doing nothing!
+
+    return out
+
+
